845ca77621ed66fbc2c4e5b49c9dc1a2e57a0aba
17mm <114557943+PsychofankakrolaJuliana@users.noreply.github.com>
2022-10-03 12:11:05 +0000

    Karta pracy 2 zad 5

---
+++ KartaPracy2.py
@@ -0,0 +1,30 @@
+# Zad.1
+a = int(input("Daj a: "))
+if a % 3 == 0:
+  print("TAK")
+else:
+  print("NIE")
+
+# Zad.2 
+a = int(input("Daj liczbe: "))
+if a % 17 == 0 and a >= 100 and a < 1000:
+  print("TAK")
+else:
+  print("NIE")
+
+# Zad.3
+a = int(input("Podaj wiek: "))
+if a>17:
+  print("Jestes pelnoletni")
+else:
+  print("Nie jestes pelnoletni")
+
+# Zad.4
+limit = 20
+waga = int(input("Podaj wage w tonach: "))
+if waga < 20:
+  print("Mozesz wjechac")
+else:
+  print("Ni mozesz wjechac")
+
+# Zad.5
