97f7b00d9188d309b32062ca06c1efbc09c7c123
17mm <114557943+PsychofankakrolaJuliana@users.noreply.github.com>
2022-10-05 05:45:56 +0000

    Zadanie 6

--- KartaPracy2.py
+++ KartaPracy2.py
@@ -1,33 +1,43 @@
 # Zad.1
-a = int(input("Daj a: "))
-if a % 3 == 0:
-  print("TAK")
-else:
-  print("NIE")
+# a = int(input("Daj a: "))
+# if a % 3 == 0:
+#   print("TAK")
+# else:
+#   print("NIE")
 
-# Zad.2 
-a = int(input("Daj liczbe: "))
-if a % 17 == 0 and a >= 100 and a < 1000:
-  print("TAK")
-else:
-  print("NIE")
+# # Zad.2 
+# a = int(input("Daj liczbe: "))
+# if a % 17 == 0 and a >= 100 and a < 1000:
+#   print("TAK")
+# else:
+#   print("NIE")
 
-# Zad.3
-a = int(input("Podaj wiek: "))
-if a>17:
-  print("Jestes pelnoletni")
-else:
-  print("Nie jestes pelnoletni")
+# # Zad.3
+# a = int(input("Podaj wiek: "))
+# if a>17:
+#   print("Jestes pelnoletni")
+# else:
+#   print("Nie jestes pelnoletni")
 
-# Zad.4
-limit = 20
-waga = int(input("Podaj wage w tonach: "))
-if waga <= 20:
-  print("Mozesz wjechac")
-else:
-  print("Ni mozesz wjechac")
+# # Zad.4
+# limit = 20
+# waga = int(input("Podaj wage w tonach: "))
+# if waga <= 20:
+#   print("Mozesz wjechac")
+# else:
+#   print("Ni mozesz wjechac")
 
 # Zad.5
-a = int(input("Daj liczbe: "))
-b = int(input("Daj liczbe: "))
-c = int(input("Daj liczbe: "))
+# a, b, c = map(int, input("Podaj 3 liczby: ").split())
+# if (a < c and b > c) or (b < c and a > c):
+#   print("TAK")
+# else:
+#   print("NIE")
+
+# Zad.6
+a = int(input("Podaj liczbe: "))
+p = int(input("Podaj liczbę pierwszą: "))
+if a**p - a % p ==0:
+  print("Tak spełnia")
+else:
+  print("Nie spełnia")
