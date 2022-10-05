b256d7d699085b48c7eeaad65b523b4cc1bc2e8f
17mm <114557943+PsychofankakrolaJuliana@users.noreply.github.com>
2022-10-05 05:54:01 +0000

    Karta pracy 2 zrobiona

--- KartaPracy2.py
+++ KartaPracy2.py
@@ -1,43 +1,52 @@
 # Zad.1
-# a = int(input("Daj a: "))
-# if a % 3 == 0:
-#   print("TAK")
-# else:
-#   print("NIE")
+a = int(input("Daj a: "))
+if a % 3 == 0:
+  print("TAK")
+else:
+  print("NIE")
 
 # # Zad.2 
-# a = int(input("Daj liczbe: "))
-# if a % 17 == 0 and a >= 100 and a < 1000:
-#   print("TAK")
-# else:
-#   print("NIE")
+a = int(input("Daj liczbe: "))
+if a % 17 == 0 and a >= 100 and a < 1000:
+  print("TAK")
+else:
+  print("NIE")
 
 # # Zad.3
-# a = int(input("Podaj wiek: "))
-# if a>17:
-#   print("Jestes pelnoletni")
-# else:
-#   print("Nie jestes pelnoletni")
+a = int(input("Podaj wiek: "))
+if a>17:
+  print("Jestes pelnoletni")
+else:
+  print("Nie jestes pelnoletni")
 
 # # Zad.4
-# limit = 20
-# waga = int(input("Podaj wage w tonach: "))
-# if waga <= 20:
-#   print("Mozesz wjechac")
-# else:
-#   print("Ni mozesz wjechac")
+limit = 20
+waga = int(input("Podaj wage w tonach: "))
+if waga <= 20:
+  print("Mozesz wjechac")
+else:
+  print("Ni mozesz wjechac")
 
 # Zad.5
-# a, b, c = map(int, input("Podaj 3 liczby: ").split())
-# if (a < c and b > c) or (b < c and a > c):
-#   print("TAK")
-# else:
-#   print("NIE")
+a, b, c = map(int, input("Podaj 3 liczby: ").split())
+if (a < c and b > c) or (b < c and a > c):
+  print("TAK")
+else:
+  print("NIE")
 
 # Zad.6
 a = int(input("Podaj liczbe: "))
 p = int(input("Podaj liczbę pierwszą: "))
-if a**p - a % p ==0:
+if (a**p - a) % p ==0:
   print("Tak spełnia")
 else:
   print("Nie spełnia")
+
+# Zad.7
+p = int(input("Podaj punkt start: "))
+k = int(input("Podaj punkt końcowy: "))
+s = int(input("Podaj dlugosc skoku: "))
+if 3*s + p >= k:
+  print("Skoczy")
+else:
+  print("Nie skoczy")
