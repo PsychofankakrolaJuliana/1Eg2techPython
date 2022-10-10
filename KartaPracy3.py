76cd008cd3a51bf4e305381232c816700c0ab7a5
17mm <114557943+PsychofankakrolaJuliana@users.noreply.github.com>
2022-10-10 12:14:42 +0000

    etgexd

---
+++ KartaPracy3.py
@@ -0,0 +1,19 @@
+# Zadanie 1
+# p = int(input("Daj liczbe "))
+# q = int(input("Daj drugą liczbe "))
+# if q * 0.3 == p:
+#   print("TAK")
+# else:
+#   print("NIE")
+
+# pętla 1
+# for i in range(10,22): print(i, end="\n")
+# pętla 2
+# for i in range(15, 32, 2): print(i, end="\n")
+# pętla 3
+for i in range(99, 10, -1): print(i, end=" ")
+
+# # Zad.1
+# n = int(input("Daj liczbe "))
+# for i in range(n):
+# print(i**3 + 3)
