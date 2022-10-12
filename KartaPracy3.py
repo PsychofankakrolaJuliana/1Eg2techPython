66622fc6a3c537b37161fe37325bad05c0ed259a
17mm <114557943+PsychofankakrolaJuliana@users.noreply.github.com>
2022-10-12 06:11:56 +0000

    zadanie 5

--- KartaPracy3.py
+++ KartaPracy3.py
@@ -1,4 +1,4 @@
-# Zadanie 1
+# ćwiczenie 1
 # p = int(input("Daj liczbe "))
 # q = int(input("Daj drugą liczbe "))
 # if q * 0.3 == p:
@@ -11,9 +11,37 @@
 # pętla 2
 # for i in range(15, 32, 2): print(i, end="\n")
 # pętla 3
-for i in range(99, 10, -1): print(i, end=" ")
+# for i in range(99, 10, -1): print(i, end=" ")
+# pętla 4
+# for i in range(10,100,1) : print(109-i, end=" ")
+# pętla 5
+# for i in range(100,1000,20) : print(i, end=" ")
 
-# # Zad.1
+# Zad.1
 # n = int(input("Daj liczbe "))
 # for i in range(n):
 # print(i**3 + 3)
+
+# Zad.2
+# for i in range(105,1000,15) :
+#   print(i, end=" ")
+
+# Zad.3
+# n = int(input("Daj liczbe: "))
+# for i in range(1,n+1) :
+#   if n % i == 0 :
+#     print(i, end=" ")
+
+# Zad.4
+# suma = 0
+# for i in range(10,100) :
+#   suma = suma + i
+# print(suma),
+
+# Zad.5
+# n = int(input("Podaj liczbe: "))
+# suma = n*(n+1)//2
+# for i in range(n-1):
+#   x = int(input("Podaj liczbe: "))
+#   suma = suma - x
+#   print("Nie podałeś: ",suma, end=" ")
