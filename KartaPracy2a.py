7d7f8f7690f92c8dafeffdbd68c655222df44976
17mm <114557943+PsychofankakrolaJuliana@users.noreply.github.com>
2022-10-15 12:44:04 +0000

    karta pracy 2a zad 1 i 2

---
+++ KartaPracy2a.py
@@ -0,0 +1,23 @@
+# Zad.1
+# a = int(input("Daj liczbe: "))
+# b = int(input("Daj drugą liczbe: "))
+# if (a+b)%2==0 :
+#   print("Suma jest parzysta")
+# else :
+#   print("Suma nie jest parzysta")
+
+# Zad.2
+# import math
+# a = int(input("Daj liczbe: "))
+# b = int(input("Daj drugą liczbe: "))
+# art = (a+b)/2
+# g = math. sqrt(a*b)
+# if art > g :
+#   print("TAK")
+# else :
+#   print("NIE")
+
+# Zad.3
+k = int(input("Daj liczbe: "))
+l = int(input("Daj drugom liczbe: "))
+m = int(input("Daj trzeciom liczbe: "))
