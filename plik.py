f392910bb27710b0fd12b03011997d3307f271dc
17mm <114557943+PsychofankakrolaJuliana@users.noreply.github.com>
2022-10-17 12:14:55 +0000

    awdwadw

---
+++ plik.py
@@ -0,0 +1,16 @@
+# POST - utrwalenie pętli
+
+# Pętle for liczb trzycyforowych podzielnych przez 13 (2 sposoby)
+# for i in range(104,1000,13) : print(i)
+# for i in range(100,100):
+#   if i % 13 == 0 :
+#     print(i)
+# Pętle for liczb dwucyfrowych parzystych (3 sposoby)
+# for i in range(10,100,2) : print(i)
+# for i in range(10,100) :
+#   if i%2==0 :
+#     print(i)
+# for i in range(5,50) :
+#   print(i*2)
+# Pętle for potęg cyfr: WY: 0, 1, 4, 9, 16 .. 81 (2 sposoby printa)
+for i in range(0,82,) : print(i)
