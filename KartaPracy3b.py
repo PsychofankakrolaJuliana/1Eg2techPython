5f90ee345af430d2ab887d6ce0b822dd51cf8274
17mm <114557943+PsychofankakrolaJuliana@users.noreply.github.com>
2022-10-22 13:15:51 +0000

    jsdnxhjacvfa

---
+++ KartaPracy3b.py
@@ -0,0 +1,21 @@
+# Zad.1
+# for i in range(1,31) :
+#   print(i, end=" ")
+
+# Zad.2
+# for i in range(1,20,2) :
+#   print(i*i, end=" ")
+
+# Zad.3
+# for i in range(1000,9999) :
+#   if i%379==0 :
+#     print(i, end=" ")
+
+# Zad.4
+# for i in range(100,1000) :
+#   if i%5==0 and i%6==0 or i%11==0 :
+#     print(i, end=" ")
+
+# Zad.5
+n = int(input("Podaj ile liczb podasz: "))
+for i in range(0,n) :
