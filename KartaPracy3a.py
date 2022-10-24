cd4231fc10f424201e27f5ed0198980bf6eb70e8
17mm <114557943+PsychofankakrolaJuliana@users.noreply.github.com>
2022-10-24 12:15:41 +0000

    dghfsem

--- KartaPracy3a.py
+++ KartaPracy3a.py
@@ -11,3 +11,20 @@
 #   else :
 #     print("--", end=" ")
 # Zad.3
+# n = int(input("Podaj n: "))
+# for i in range(1,n+1):
+#   print("*", end=" ")
+#   if i%2==1 :
+#       print("--"*i, end=" ")
+#   else :
+#       print("|"*i, end=" ")
+# Zad.5
+n = int(input("Podaj n: "))
+for i in range(1,n) :
+  print("*", end=" ")
+  print(" "*i, end=" ")
+  print("*")
+
+# for i in range(1,11):
+#   for j in range(1,11) :
+#     print(i*j, end="\t")
