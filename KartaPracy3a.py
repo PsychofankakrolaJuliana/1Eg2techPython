f875a38af85451b718eeef7b1b663ca21e5ccf1b
16mm <114557943+PsychofankakrolaJuliana@users.noreply.github.com>
2022-10-26 06:03:36 +0000

    asdfghjk

--- KartaPracy3a.py
+++ KartaPracy3a.py
@@ -18,13 +18,39 @@
 #       print("--"*i, end=" ")
 #   else :
 #       print("|"*i, end=" ")
-# Zad.5
-n = int(input("Podaj n: "))
-for i in range(1,n) :
-  print("*", end=" ")
-  print(" "*i, end=" ")
-  print("*")
+# Zad.4
+# n = int(input("Podaj n: "))
+# print("*")
+# for i in range(0,n) :
+#   print(" "*n, "*", end=" ")
+#   print(" "*i, end=" ")
+#   print("*")
+# for j in range(n,0,-1):
+#   print(" "*n, "*", end=" ")
+#   print(" "*j, end=" ")
+#   print("*")
+# print(" "*j, "*")
 
 # for i in range(1,11):
 #   for j in range(1,11) :
 #     print(i*j, end="\t")
+
+# 2 petle
+# *
+# **
+# ***
+n=int(input("Podaj n: "))
+h=int(input("Podaj h: "))
+for i in range(n):
+  for j in range(i+1):
+    print("*", end=" ")
+  print()
+print("* "*h)
+
+# ***
+# **
+# *
+for i in range(n):
+   for j in range(n-i):
+     print("*", end=" ")
+   print()
---
+++ plik1.py
@@ -0,0 +1,6 @@
+n = int(input("Podaj n: "))
+suma=0
+for i in range(0,n):
+  a=int(input("Podaj liczbe: "))
+  suma=suma+a
+print(suma)
