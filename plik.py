7eccdbf68f96a694d0d3971809d048e623996603
16mm <114557943+PsychofankakrolaJuliana@users.noreply.github.com>
2022-11-01 20:19:21 +0000

    hfjc

--- plik.py
+++ plik.py
@@ -37,6 +37,19 @@
 #     print("|"*i,"*", end=" ")
 #   else:
 #     print("-"*i, "*", end=" ")
+
+# Zad.5
+n = int(input("Podaj n: "))
+for i in range(1,n+1):
+  for j in range(1,n):
+    if (n%2==0 and j==n/2+1) or (n%2>0 and j==n/2+0.5)  :
+      print("*", end=" ")
+    elif (n%2==0 and i==n/2+1) or (n%2>0 and i==n/2+0.5) :
+      print("-", end=" ")
+    else:
+      print(" ", end=" ")
+  print()
+
 # Zad.7
 # n = int(input("Podaj n: "))
 # for i in range(1,n+1):
