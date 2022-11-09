38ad4943292b7a1636624544a31829184b315e58
16mm <114557943+Qniara@users.noreply.github.com>
2022-11-09 07:08:50 +0000

    hgvhk

--- KartaPracy3a.py
+++ KartaPracy3a.py
@@ -26,15 +26,11 @@ for i in range(1,n+1):
     if i==n-j+1:
       print("*",  end=" ")
     # elif i==1 and (n%2==0 and j==n/2) or (n%2>0 and j==n/2+0.5):
+    elif j==i:
       print("*", end=" ")
     else:
       print("-", end=" ")
   print()
-
-# n=int(input("Podaj n: "))
-# for i in range(1,n+1):
-#   for j in range(1,j)
-
 # Zad.5
 # n = int(input("Podaj n: "))
 # for i in range(1,n+1):
