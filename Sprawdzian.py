32646fa651e42feada022e0f1802ec0f03368dfe
16mm <114557943+Qniara@users.noreply.github.com>
2022-11-16 06:21:20 +0000

    fdesv

--- Sprawdzian.py
+++ Sprawdzian.py
@@ -1,9 +1,9 @@
 # Zad.1
-suma = 0
-for i in range(100,1000,5):
-  suma = suma+i 
-suma = suma/2
-print(suma)
+# suma = 0
+# for i in range(100,1000,5):
+#   suma = suma+i 
+# suma = suma/2
+# print(suma)
 
 # Zad.2
 # n = int(input("Podaj n: "))
@@ -17,12 +17,12 @@ print(suma)
 # print(wynik)
 
 # Zad.3
-# n =int(input("Podaj n: ")) 
-# for i in range(1,n+1):
-#   for j in range(1,n+1):
-#     # if :
-#     #   print("*", end=" ")
-#     if j==n/2:
-#       print("***")
-# print()  
-# print("*"*n)
+n =int(input("Podaj n: ")) 
+for i in range(1,n+1):
+  for j in range(1,n+1):
+    # if :
+    #   print("*", end=" ")
+    if j==n/2:
+      print("***")
+print()  
+print("*"*n)
