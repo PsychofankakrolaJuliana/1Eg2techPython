ffacf94843c43099f8472996ba3ee8c6ee061c98
16mm <114557943+Qniara@users.noreply.github.com>
2022-11-16 06:36:13 +0000

    hygvyuhkv

---
+++ Algorytmy1/Zadanie.py
@@ -0,0 +1,8 @@
+# 1 Algorytm sprawdzający czy liczba jest pierwsza
+n = int(input("Podaj liczbe: "))
+for i in range(1,n+1):
+  if n/i==0:
+    
+# 2. Generator liczb pierwszych w przedziale [p,q]
+
+# 3.Generator liczb pierwszych - początkowe k liczb pierwszych 
