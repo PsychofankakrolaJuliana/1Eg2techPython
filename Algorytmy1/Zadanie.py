dd7e6b7eab33c729e1d450d731f9c53b48f4e113
16mm <114557943+Qniara@users.noreply.github.com>
2022-11-21 13:08:12 +0000

    wdew

--- Algorytmy1/Zadanie.py
+++ Algorytmy1/Zadanie.py
@@ -32,18 +32,29 @@
 # print("Liczba jest pierwsza")
 
 # 2. Generator liczb pierwszych w przedziale [p,q]
-p = int(input("Podaj liczbe: "))
-q = int(input("Podaj liczbe koncowa: "))
-for i in range(2,q+1):
-  flaga = True
-  for j in range(2,int(i**0.5)+1):
-    if j%i==0:
-      flaga = False
-if flaga:
-  print(i, end=" ")
+# p, q = map(int, input("Podaj 2 liczby: ").split())
+# for i in range(p,q+1):
+#   flaga = True
+#   for j in range(2,int(i**0.5)+1):
+#     if j % i == 0:
+#       flaga = False
+#   if flaga:
+#     print(i, end=" ")
 
 # 3.Generator liczb pierwszych - początkowe k liczb pierwszych 
-# k = int(input("Podaj ile chcesz liczb pierwszych: "))
-# for i in range():
-#   if i
-#   print((i)*k)
+k = int(input("Podaj ile chcesz liczb pierwszych: "))
+x = 2
+while 1:
+  for i in range(2,x):
+    if x % i == 0:
+      flaga == False
+  if flaga:
+    print(x, end=" ")
+    k = k-1
+  if k == 0:
+    break
+x = x+1
+
+# SPLIT
+# m = "Niech zyje krol Julian".split()
+# print(m)
