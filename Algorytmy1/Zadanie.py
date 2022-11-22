a088559cbdc6874ffdbd0795049b623715d6850c
16mm <114557943+Qniara@users.noreply.github.com>
2022-11-22 18:57:26 +0000

    fger

--- Algorytmy1/Zadanie.py
+++ Algorytmy1/Zadanie.py
@@ -29,33 +29,41 @@
 #   if n%i==0:
 #     print("Liczba nie jest pierwsza")
 #     exit(0)
-# print("Liczba jest pierwsza")
+#   else:
+#     print("Liczba jest pierwsza")
 
 # 2. Generator liczb pierwszych w przedziale [p,q]
-# p, q = map(int, input("Podaj 2 liczby: ").split())
-# for i in range(p,q+1):
-#   flaga = True
-#   for j in range(2,int(i**0.5)+1):
-#     if j % i == 0:
-#       flaga = False
-#   if flaga:
-#     print(i, end=" ")
+from math import sqrt
+p, q = map(int, input("Podaj 2 liczby: ").split())
+k = 1
+for i in range(p,q+1):
+  flaga = True
+  k = sqrt(i)
+  l=round(k,1)
+  l=l+1
+  print(l)
+  for j in range(2, k):
+    if i % j == 0:
+      flaga = False
+  if flaga==True:
+    print(i, end=" ")
 
 # 3.Generator liczb pierwszych - początkowe k liczb pierwszych 
-k = int(input("Podaj ile chcesz liczb pierwszych: "))
-x = 2
-while 1:
-  flaga = True
-  for i in range(2, int(x**0.5)+1):
-    if x % i == 0:
-      flaga = False
-  if flaga:
-    print(x, end=" ")
-    k = k-1
-    if k == 0:
-      break
-  x = x+1
+# k = int(input("Podaj ile chcesz liczb pierwszych: "))
+# x = 2
+# while 1:
+#   flaga = True
+#   for i in range(2, int(x**0.5)+1):
+#     if x % i == 0:
+#       flaga = False
+#   if flaga:
+#     print(x, end=" ")
+#     k = k-1
+#     if k == 0:
+#       break
+#   x = x+1
 
 # SPLIT
 # m = "Niech zyje krol Julian".split()
 # print(m)
+# round((i**0.5),0)+1)
