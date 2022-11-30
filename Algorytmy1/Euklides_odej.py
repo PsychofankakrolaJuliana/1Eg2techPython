2b4ebfe510f6e9d5a9f1aa959304de621513667e
15mm <114557943+Qniara@users.noreply.github.com>
2022-11-30 06:34:07 +0000

    gh

--- Algorytmy1/Euklides_odej.py
+++ Algorytmy1/Euklides_odej.py
@@ -6,3 +6,21 @@
 #   if a>b: a=a-b
 #   elif b>a: b=b-a
 # print(a)
+
+# NWW
+# Sposób 1
+# a = int(input("Podaj a: "))
+# b = int(input("Podaj b: "))
+# c =0 
+# while a!=b:
+#   c=a*b
+# while a!=b:
+#   if a>b: 
+#     a=a-b
+#     c=c/a
+#   elif b>a: 
+#     b=b-a
+#     c=c/b
+# print(c)
+
+# Sposób 2
