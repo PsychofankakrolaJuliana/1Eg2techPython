961fd25c72db5b4659334eee9b95dcccb421b280
15mm <114557943+Qniara@users.noreply.github.com>
2022-12-05 13:01:38 +0000

    wervqwr

---
+++ Cezar/Pre.py
@@ -0,0 +1,17 @@
+# Szyfr Cezara
+#  A B C D E F G H  I J K L M N O P Q R S T U V W X Y Z
+# Funkcja ord - zwraca kod ascii dla danego znaku
+# Funkcja chr - zwraca znak dla kodu ascii
+# print(ord("M"))
+# print(chr(69))
+# Można połączyć
+# print(chr(ord("C")+5))
+
+# Napisz alfabet za pomocą pętli for
+# for i in range(65,91):
+#   print(chr(i), end=" ")
+
+# Jak wydobyć literki z napisu
+napis = "Krecio"
+print(len(napis))
+print(napis[0])
