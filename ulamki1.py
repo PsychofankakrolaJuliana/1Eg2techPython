4f8cbae1d9dab3104875ec472482edd78ab6aa73
15mm <114557943+Qniara@users.noreply.github.com>
2022-12-07 06:57:41 +0000

    bgdryrc

--- ulamki1.py
+++ ulamki1.py
@@ -1,12 +1,14 @@
-a = int(input("Podaj liczbe: "))
-b = int(input("Podaj liczbe: "))
-c = int(input("Podaj liczbe: "))
-d = int(input("Podaj liczbe: "))
-x = b
-y = d
-ilo = x*y
-while y>0:
-  x, y = y, x%y
-nww = ilo // x
-a=a*(nww/b)
-print(f"{a}/{b}+{c}/{d}")
+# a = int(input("Podaj liczbe: "))
+# b = int(input("Podaj liczbe: "))
+# c = int(input("Podaj liczbe: "))
+# d = int(input("Podaj liczbe: "))
+# x = b
+# y = d
+# ilo = x*y
+# while y>0:
+#   x, y = y, x%y
+# nww = ilo // x
+# e = (nww//b)*a
+# f = (nww//d)*c
+# g=e+f
+# print(f"{a}/{b}+{c}/{d}={e}/{nww}+{f}/{nww}={g}/{nww}")
