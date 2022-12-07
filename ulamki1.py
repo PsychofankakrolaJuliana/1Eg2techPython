7a5b41eee59951179b44490450301bd346da7f9b
15mm <114557943+Qniara@users.noreply.github.com>
2022-12-07 06:49:40 +0000

    etgbest

--- ulamki1.py
+++ ulamki1.py
@@ -2,3 +2,11 @@ a = int(input("Podaj liczbe: "))
 b = int(input("Podaj liczbe: "))
 c = int(input("Podaj liczbe: "))
 d = int(input("Podaj liczbe: "))
+x = b
+y = d
+ilo = x*y
+while y>0:
+  x, y = y, x%y
+nww = ilo // x
+a=a*(nww/b)
+print(f"{a}/{b}+{c}/{d}")
