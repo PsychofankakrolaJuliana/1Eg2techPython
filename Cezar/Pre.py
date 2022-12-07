3553a369077548cf83acffa3295dd78b2f245218
15mm <114557943+Qniara@users.noreply.github.com>
2022-12-07 06:46:09 +0000

    tbrdyd

--- Cezar/Pre.py
+++ Cezar/Pre.py
@@ -32,3 +32,47 @@
 # for i in range(len(napis)):
 #   szyfr = szyfr + chr(65 + (ord(napis[i]) - 65 + 3) % 26)
 # print(szyfr)
+
+# dodaj ułamki o różnych mianownikach
+# a = int(input("Podaj liczbe: "))
+# b = int(input("Podaj liczbe: "))
+# c = int(input("Podaj liczbe: "))
+# d = int(input("Podaj liczbe: "))
+# e= 0
+# f=0
+# g=0
+# h=0
+# while a!=b:
+#   if a>b: e=a-b
+#   elif b>a: e=b-a
+# while c!=d:
+#   if c>d: f=c-d
+#   elif d>c: f=d-c
+# a=a/e
+# b=b/e
+# c=c/f
+# d=d/f
+# while a!=c: g=a*c
+# while a!=b:
+#   if a>b: 
+#     a=a-c 
+#     h=h/a
+#   elif c>a: 
+#     c=c-a
+#     h=h/c
+# while b!=d:
+#   h=b*d
+# while b!=d:
+#   if b>d: 
+#     b=b-d
+#     h=h/a
+#   elif d>b: 
+#     d=d-b
+#     h=h/d
+# b=d
+# a=c
+# suma=0
+# sum = 0
+# suma = a+c
+# sum = b+d
+# print(suma,"/",sum)
---
+++ ulamki1.py
@@ -0,0 +1,4 @@
+a = int(input("Podaj liczbe: "))
+b = int(input("Podaj liczbe: "))
+c = int(input("Podaj liczbe: "))
+d = int(input("Podaj liczbe: "))
