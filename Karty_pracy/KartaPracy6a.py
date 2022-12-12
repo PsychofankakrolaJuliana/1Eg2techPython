e2d2fa453cab1b34d6cc2ad7c5a2686c69f5a3b0
15mm <114557943+Qniara@users.noreply.github.com>
2022-12-12 13:08:01 +0000

    vsetdtbgd

--- Karty_pracy/KartaPracy6a.py
+++ Karty_pracy/KartaPracy6a.py
@@ -1,7 +1,43 @@
 # Zad.1
-x  = int(input("Podaj liczbę: "))
-suma=0
-while x>0:
-  suma=suma+x%10
-  x=x//10
-print(suma)
+# x  = int(input("Podaj liczbę: "))
+# suma=0
+# while x>0:
+#   suma=suma+x%10
+#   x=x//10
+# print(suma)
+# Zad.2
+# n = int(input("Podaj liczbe: "))
+# for i in range(2,n):
+#   if n%i==0:
+#     print("Podana liczba nie jest pierwsza")
+#   else:
+#     print("Podana liczba nie jest pierwsza")
+# Zad.3
+# n = int(input("Podaj liczbe: "))
+# suma=0
+# for i in range(1,n):
+#   if n%i==0:
+#     suma=suma+i
+# if suma==n:
+#   print("Liczba jest doskonała")
+# else:
+#   print("Nie jest doskonała ta liczba")
+# Zad.4
+# a = int(input("Podaj liczbę: "))
+# b = int(input("Podaj drugą liczbe: "))
+# while a!=b:
+#   if a>b: a=a-b
+#   elif b>a: b=b-a
+# if a==1:
+#   print("Podane liczby są względnie pierwsze")
+# else:
+#   print("Podane liczby nie są względnie pierwsze")
+# Zad.5
+m = int(input("Podaj liczbe: "))
+for i in range(10,19):
+  x==n
+  y==i
+  while y >0:
+    x,y=y, x%y
+  nwd = x
+  print()
