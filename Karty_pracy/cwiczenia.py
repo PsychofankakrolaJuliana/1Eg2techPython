91979fd21bcb809e3042716a28353d41be496ed3
15mm <114557943+Qniara@users.noreply.github.com>
2022-12-18 16:57:03 +0000

    jgvyi

--- Karty_pracy/cwiczenia.py
+++ Karty_pracy/cwiczenia.py
@@ -27,16 +27,29 @@
 # else:
 #   print("Podana liczba nie jest doskonala")
 # Zad.4
-a = int(input("Podaj liczbe: "))
-b = int(input("Podaj druga liczbe: "))
-x=a
-y=b
-while x!=y:
-  if x>y:
-    x=x-y
-  elif y>x:
-    y=y-x
-if x==1:
-  print("Podane liczby sa wzglednie pierwsze")
-else:
-  print("Podane liczby nie sa wzglednie pierwsze")
+# a = int(input("Podaj liczbe: "))
+# b = int(input("Podaj druga liczbe: "))
+# x=a
+# y=b
+# while x!=y:
+#   if x>y:
+#     x=x-y
+#   elif y>x:
+#     y=y-x
+# if x==1:
+#   print("Podane liczby sa wzglednie pierwsze")
+# else:
+#   print("Podane liczby nie sa wzglednie pierwsze")
+# Zad.5
+# n = int(input("Podaj liczbe: "))
+# x=n
+# y=0
+# for i in range(10,20):
+#   i=y
+#   while x!=y:
+#     if x>y:
+#       x=x-y
+#     elif y>x:
+#       y=y-x
+#     if x==0:
+#       print(i, end=" ")
