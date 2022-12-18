92273954c0d0a8360cd1110b4b9f85046fa5cd9c
15mm <114557943+Qniara@users.noreply.github.com>
2022-12-18 15:53:50 +0000

    dgdr

--- Karty_pracy/cwiczenia.py
+++ Karty_pracy/cwiczenia.py
@@ -6,3 +6,12 @@
 #   suma=suma+x%10
 #   x=x//10
 # print(suma)
+# Zad.2
+a = int(input("Podaj liczbe: "))
+while a > 0:
+  for i in range(2, a):
+    if a % i == 0:
+      print("Podana liczba nie jest pierwsza")
+      break
+    else:
+      print("Podana liczba jest pierwsza")
