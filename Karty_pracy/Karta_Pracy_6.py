bcd4cd2edf36d20f3e7b9e3317891da1e1841127
15mm <114557943+Qniara@users.noreply.github.com>
2022-12-29 06:23:07 +0000

    adx\awe

--- Karty_pracy/Karta_Pracy_6.py
+++ Karty_pracy/Karta_Pracy_6.py
@@ -1,4 +1,11 @@
 # Zad.1
-a = int(input("Podaj liczbe: "))
-b = int(input("Podaj drugom liczbe: "))
-c = int(input("Podaj trzeciom liczbe: "))
+# a = int(input("Podaj liczbe: "))
+# b = int(input("Podaj drugom liczbe: "))
+# c = int(input("Podaj trzeciom liczbe: "))
+# if()
+# Zad.2
+suma = 0
+for i in range(100,1000):
+  if i%8==0 and i%16==1:
+    suma=suma+i
+print(suma)
