0365460d1f7fa782c08f69b4c172c3606d05ca7e
15mm <114557943+Qniara@users.noreply.github.com>
2022-12-29 06:26:43 +0000

    drfsvrdeg

--- Karty_pracy/Karta_Pracy_6.py
+++ Karty_pracy/Karta_Pracy_6.py
@@ -4,8 +4,14 @@
 # c = int(input("Podaj trzeciom liczbe: "))
 # if()
 # Zad.2
-suma = 0
-for i in range(100,1000):
-  if i%8==0 and i%16==1:
-    suma=suma+i
-print(suma)
+# suma = 0
+# for i in range(100,1000):
+#   if i%8==0 and i%16!=0:
+#     suma=suma+i
+# print(suma)
+# Zad.3
+x=0
+suma=0
+for i in range(10,99,-1):
+  if i%7==0:
+    x=i
