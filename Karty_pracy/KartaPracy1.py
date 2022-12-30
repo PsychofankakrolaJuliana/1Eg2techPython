b40960be9c19a8bc952098383da520ced2a45be9
15mm <114557943+Qniara@users.noreply.github.com>
2022-12-30 06:38:11 +0000

    ascdsvf

--- Karty_pracy/KartaPracy1.py
+++ Karty_pracy/KartaPracy1.py
@@ -1,35 +1,35 @@
 # Zad.1
-a=int(input())
-b=int(input())
-print(a**2+b**2)
+# a=int(input())
+# b=int(input())
+# print(a**2+b**2)
 
 # Zad.2
-c=int(input())
-d=int(input())
-print((a+b)**2)
+# c=int(input())
+# d=int(input())
+# print((a+b)**2)
 
 # Zad.3
-e=int(input())
-f=int(input())
-print((e-f)**3)
+# e=int(input())
+# f=int(input())
+# print((e-f)**3)
 
 # Zad.4
-g=int(input())
-h=int(input())
-i=int(input())
-print(g*h*i)
+# g=int(input())
+# h=int(input())
+# i=int(input())
+# print(g*h*i)
 
 # Zad.5
-j=float(input())
-k=float(input())
-print(2(j+k)*0,2)
+# j=float(input())
+# k=float(input())
+# print(2(j+k)*0,2)
 
 # Zad.6
-l=float(input())
-m=l-l*0,23
-print(m)
+# l=float(input())
+# m=l-l*0,23
+# print(m)
 
 # Zad.7
-n=int(input())
-o=int(input())
-print((n%o)
+# n=int(input())
+# o=int(input())
+# print((n%o)
