c430f38bbb1a86e6c262a254589612458ccdd202
15mm <114557943+Qniara@users.noreply.github.com>
2023-01-15 19:43:11 +0000

    csdfx

--- Petla_While/KP_3B.py
+++ Petla_While/KP_3B.py
@@ -1,20 +1,23 @@
 # Zad.1
 # i=0
-# while(i<30):
+# while(i<=30):
 #   i+=1
 #   print(i)
 
 # Zad.2
 # i=-1
-# while i<9:
+# while i<=9:
 #   i+=2
 #   print(i**2)
 
 # Zad.3
-i=1000
-while i<9999:
-  if(i%379==0):
-    print(i)
-  i=i+1
+# i=1000
+# while i<=9999:
+#   if(i%379==0):
+#     print(i)
+#   i=i+1
 
 # Zad.4
+i=100
+while i<=999:
+  
