9f9d03b56d78e7a847dd09de475959d6d474774c
15mm <114557943+Qniara@users.noreply.github.com>
2023-01-15 19:42:23 +0000

    sevfxd

--- Petla_While/KP_3B.py
+++ Petla_While/KP_3B.py
@@ -3,12 +3,18 @@
 # while(i<30):
 #   i+=1
 #   print(i)
+
 # Zad.2
 # i=-1
 # while i<9:
 #   i+=2
 #   print(i**2)
+
 # Zad.3
 i=1000
 while i<9999:
-  if(i%379)
+  if(i%379==0):
+    print(i)
+  i=i+1
+
+# Zad.4
