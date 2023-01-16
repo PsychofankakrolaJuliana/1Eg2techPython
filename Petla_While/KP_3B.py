79bc10b4c6f64330a5f4033af3de15ef8c9f16b8
15mm <114557943+Qniara@users.noreply.github.com>
2023-01-16 13:43:12 +0000

    ghvb

--- Petla_While/KP_3B.py
+++ Petla_While/KP_3B.py
@@ -44,10 +44,15 @@
 # print("Suma", k, "liczb jest rowna: ", suma)
 
 # Zad.7
-m =int(input("Podaj liczbe: "))
-i = 11
-suma=0
-while i < m:
-  i = i + 2
-  suma=suma+i
-print("Suma", m, "liczb jest rowna: ", suma)
+# m =int(input("Podaj liczbe: "))
+# i = 11
+# suma=0
+# while i < m:
+#   i = i + 2
+#   suma=suma+i
+# print("Suma", m, "liczb jest rowna: ", suma)
+
+# Zad.9
+n=int(input("Podaj liczbe: ")))
+i=0
+while i<
