2bf91e3d8e715f14546d19e8155dc09a752c23ab
15mm <114557943+Qniara@users.noreply.github.com>
2023-01-18 07:25:54 +0000

    5rfgh76yt

--- Listy/Listy.py
+++ Listy/Listy.py
@@ -1,17 +1,53 @@
-# Printowanie listy
-x=list(range(5))
-for item in x:
-  print(item, end=" ")
+# # Printowanie listy
+# x=list(range(5))
+# for item in x:
+#   print(item, end=" ")
 
-# Len
-for i in range(len(x)):
-  print(x[i], end=" ")
 
-# print(len(x))
+# # Len
+# for i in range(len(x)):
+#   print(x[i], end=" ")
 
-# Printowanei całej listy
-L = [1,2,3,4,5]
-for elem in L:
-  print(elem, end=" ")
-for i in range(len(L)):
-  print(L[i], end=" ")
+# # print(len(x))
+
+# # Printowanei całej listy
+# L = [1,2,3,4,5]
+# for elem in L:
+#   print(elem, end=" ")
+# for i in range(len(L)):
+#   print(L[i], end=" ")
+
+# # Funkcje w lisatch
+K=[4,7,5,7,3,3,2,8,7]
+# print(K)
+
+# K.append(3)
+# print(K)
+
+# print(K.count(7))
+
+# print(K.index(7))
+
+# K.insert(2,4)
+# print(K)
+
+# K.pop()
+# print(K)
+
+# K.pop(2)
+# print(K)
+
+# K.reverse()
+# print(K)
+
+# K.sort(reverse=True)
+# print(K)
+
+# # Usunąć wszystkie 7 z listy
+# for i in range(K.count(7)):
+#   K.remove(7)
+# print(K)
+
+# for i in range(K.count(7)):
+#   K.pop(K.index(7))
+# print(K)
