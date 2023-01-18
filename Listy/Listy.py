08cb3fcccf852ae33ef9574440d4fba2ba6328ad
15mm <114557943+Qniara@users.noreply.github.com>
2023-01-18 06:46:20 +0000

    dftgbc

---
+++ Listy/Listy.py
@@ -0,0 +1,17 @@
+# Printowanie listy
+x=list(range(5))
+for item in x:
+  print(item, end=" ")
+
+# Len
+for i in range(len(x)):
+  print(x[i], end=" ")
+
+# print(len(x))
+
+# Printowanei całej listy
+L = [1,2,3,4,5]
+for elem in L:
+  print(elem, end=" ")
+for i in range(len(L)):
+  print(L[i], end=" ")
