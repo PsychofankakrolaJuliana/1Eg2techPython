dd1b69ab619250be11fef85a7af604d91f6b74b5
15mm <114557943+Qniara@users.noreply.github.com>
2023-01-19 17:53:17 +0000

    esrfdtgdxe

--- Petla_While/KP_py/KP_6A.py
+++ Petla_While/KP_py/KP_6A.py
@@ -6,3 +6,28 @@
 #   n=n//10
 # print(suma)
 
+# Zad.2
+# n=int(input("Podaj liczbe: "))
+# i=2
+# F=1
+# while i<n:
+#   if n%i==0:
+#     F=0
+#   i=i+1
+# if F==1:
+#   print("Podana liczba jest pierwsza")
+# elif F==0:
+#   print("Podana liczba nie jest pierwsza")
+
+# Zad.3
+# suma=0
+# n=int(input("Podaj liczbe: "))
+# i=1
+# while i<n:
+#   if n%i==0:
+#     suma=suma+i
+#   i=i+1
+# if suma==n:
+#   print("Podana liczba jest doskonala")
+# else:
+#   print("Podana liczba nie jest doskonala")
