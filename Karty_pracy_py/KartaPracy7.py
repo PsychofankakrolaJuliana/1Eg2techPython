3710141ead793f18d36c956a2128e1d6eec307f8
15mm <114557943+Qniara@users.noreply.github.com>
2023-01-21 09:17:53 +0000

    123

--- Karty_pracy_py/KartaPracy7.py
+++ Karty_pracy_py/KartaPracy7.py
@@ -1,12 +1,13 @@
 # Karty_pracy_py//KartaPracy7.py
 import random
-for T in range(0, 40):
-  print((random.randint(10, 99)), end=" ")
+T=[0]
+for j in range(0, 40):
+  T[j]=random.randint(10, 99)
 print("\n")
 
 # Zad.1
 maks=0
-for i in range(T):
+for i in range(0,len(T)):
   if i>maks:
     maks=i
 print(maks)
