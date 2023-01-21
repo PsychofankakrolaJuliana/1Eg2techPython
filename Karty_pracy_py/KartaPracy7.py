df217c529dfbcf411e1a19c798d87e2f26536884
15mm <114557943+Qniara@users.noreply.github.com>
2023-01-21 09:32:37 +0000

    jdhsrfgbds

--- Karty_pracy_py/KartaPracy7.py
+++ Karty_pracy_py/KartaPracy7.py
@@ -1,13 +1,19 @@
 # Karty_pracy_py//KartaPracy7.py
 import random
-T=[0]
-for j in range(0, 40):
-  T[j]=random.randint(10, 99)
+T = []
+for i in range(40):
+  random_number = random.randint(10,99)
+  T.append(random_number)
+print(T, end=" ")
 print("\n")
 
 # Zad.1
-maks=0
-for i in range(0,len(T)):
-  if i>maks:
-    maks=i
-print(maks)
+# maks=0
+# for i in range(0,len(T)):
+#   if i>maks:
+#     maks=i
+# print(maks)
+
+# print(max(T))
+
+# Zad.2
