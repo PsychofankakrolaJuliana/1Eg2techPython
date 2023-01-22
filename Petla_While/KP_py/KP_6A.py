414cc06105b7e4d2efa7544114c1d7afe22db133
15mm <114557943+Qniara@users.noreply.github.com>
2023-01-22 07:50:58 +0000

    ertgdgd

--- Petla_While/KP_py/KP_6A.py
+++ Petla_While/KP_py/KP_6A.py
@@ -32,3 +32,18 @@
 #   print("Podana liczba jest doskonala")
 # else:
 #   print("Podana liczba nie jest doskonala")
+
+# Zad.4
+# a= int(input("Podaj liczbe: "))
+# b= int(input("Podaj liczbe: "))
+# x=a
+# y=b
+# while x<y:
+#   if x>y:
+#     x=x-y
+#   else:
+#     y=y-x
+# if x==1 or y==1:
+#   print("Podane liczby sa wzglednie pierwsze.")
+# else:
+#   print("Podane liczby nie sa wzglednie pierwsze.")
