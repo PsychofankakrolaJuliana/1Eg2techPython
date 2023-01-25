4691f5053adab4e8a6d40413f34699278c70405e
15mm <114557943+Qniara@users.noreply.github.com>
2023-01-25 08:09:09 +0000

    htfvby

--- Algorytmy1/hufman.py
+++ Algorytmy1/hufman.py
@@ -1,5 +1,15 @@
 # Algorytmy1//hufman.py
-W="ABCCCDDDDEFGGGHHIJJ"
-H="AB3C4DEF3G2HI2J"
-for i in range(len(W)):
-  
+W="AABCCCDDDDEFGGGHHIJJ"
+E="2AB3C4DEF3G2HI2J"
+H=""
+ilo=1
+for i in range(len(W)-1):
+  if W[i]==W[i+1]:
+    ilo=ilo+1
+  else:
+    if ilo>1:
+      H=H+str(ilo)
+    H=H+W[i]
+    ilo=1
+print(W)
+print(H)
