7136da82709c63160c18549eca7841e293a9798e
15mm <114557943+Qniara@users.noreply.github.com>
2023-01-28 15:21:45 +0000

    hgbjd

--- Algorytmy1/hufman.py
+++ Algorytmy1/hufman.py
@@ -1,18 +1,18 @@
 # Algorytmy1//hufman.py
-W="AABCCCDDDDEFGGGHHIJJ"
-E="2AB3C4DEF3G2HI2J"
-H=""
-ilo=1
-for i in range(len(W)-1):
-  if W[i]==W[i+1]:
-    ilo=ilo+1
-  else:
-    if ilo>1:
-      H=H+str(ilo)
-    H=H+W[i]
-    ilo=1
-if ilo>1:
-  H=H+str(ilo)
-H=H+W[len(W)-1]
-print(W)
-print(H)
+# W="AABCCCDDDDEFGGGHHIJJ"
+# E="2AB3C4DEF3G2HI2J"
+# H=""
+# ilo=1
+# for i in range(len(W)-1):
+#   if W[i]==W[i+1]:
+#     ilo=ilo+1
+#   else:
+#     if ilo>1:
+#       H=H+str(ilo)
+#     H=H+W[i]
+#     ilo=1
+# if ilo>1:
+#   H=H+str(ilo)
+# H=H+W[len(W)-1]
+# print(W)
+# print(H)
