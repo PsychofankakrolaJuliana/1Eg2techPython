eee696c9e41a8db5c47a8af60c995103fad66fe3
15mm <114557943+Qniara@users.noreply.github.com>
2023-02-10 08:37:04 +0000

    dgzfcvdrxghb

--- Algorytmy1/Algorytm_SA.py
+++ Algorytmy1/Algorytm_SA.py
@@ -1,6 +1,9 @@
 # Algorytm szyfrowania SA -> Algorytm asymetryczny
 # Funkcje skurutu:
 # sha md5
+# from hashlib import md5
+# print(md5(b"Gruszka1").hexdigest())
+# print(md5(b"Gruszka2").hexdigest())
+
 from hashlib import md5
-print(md5(b"Gruszka1").hexdigest())
-print(md5(b"Gruszka2").hexdigest())
+print(md5(b"Findusek Maluszek").hexdigest())
