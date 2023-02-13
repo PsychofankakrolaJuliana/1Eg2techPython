1410984f1dc59f265944c298523dfe57a3c62058
15mm <114557943+Qniara@users.noreply.github.com>
2023-02-13 12:41:45 +0000

    fdgredb

--- Algorytmy1/Algorytm_RSA.py
+++ Algorytmy1/Algorytm_RSA.py
@@ -12,7 +12,7 @@
 # F=(p-1)*(q-1)
 # n=p*q
 # print(F," ",n)
-# # 3.Znalezienie klucza publicznego e: NWD(F,e)=1
+# # 3.Znalezienie klucza publicz nego e: NWD(F,e)=1
 # from math import gcd
 # for i in range(2,F):
 #   if gcd(i,F)==1:
