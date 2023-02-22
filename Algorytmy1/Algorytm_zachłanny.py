c0f8bc86517955f51087e044cda17b187dda3647
15mm <114557943+Qniara@users.noreply.github.com>
2023-02-22 06:53:30 +0000

    hujkh

--- Algorytmy1/Algorytm_zachłanny.py
+++ Algorytmy1/Algorytm_zachłanny.py
@@ -2,18 +2,17 @@
 # Algorytm zachłanny - algorytm człowieczy
 
 # Wydawanie reszty: 
-kw=int(input("Podaj kwote: "))
-ilo=0
-x=0
-T=[500,200,100,50,20,10,5,2,1]
-while kw>0:
-  for i in T:
-    if T[i]<kw:
-      x=kw//T[i]
-      kw=kw-T[i]*x
-      print(f"{x}*{T[i]} ")
-    if kw>T[i]:
-      
+# kw=int(input("Podaj kwote: "))
+# ilo=0
+# x=0
+# T=[500,200,100,50,20,10,5,2,1]
+# while kw>0:
+#   for i in T:
+#     if T[i]<kw:
+#       x=kw//T[i]
+#       kw=kw-T[i]*x
+#       print(f"{x}*{T[i]} ") 
+#     x=0
 
 # kw=int(input("Podaj kwote: "))
 # ilo=0
@@ -22,3 +21,4 @@ while kw>0:
 #   ilo=kw//i
 #   kw=kw-ilo*i
 #   print(f"{i}, {ilo}")
+
