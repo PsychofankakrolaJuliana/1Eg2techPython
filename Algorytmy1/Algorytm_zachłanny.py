7efc7f80b069ae3eb53ee6e53821cec27c38d624
15mm <114557943+Qniara@users.noreply.github.com>
2023-02-22 06:43:07 +0000

    uhrkghlg

--- Algorytmy1/Algorytm_zachłanny.py
+++ Algorytmy1/Algorytm_zachłanny.py
@@ -2,3 +2,11 @@
 # Algorytm zachłanny - algorytm człowieczy
 
 # Wydawanie reszty: 
+kw=int(input("Podaj kwote: "))
+ilo=0
+x=0
+T=[1,2,5,10,20,50,100,200,500]
+while kw>0:
+  for i in T:
+    if T[i]>kw:
+      x=kw//T[i]
