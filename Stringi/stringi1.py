73e88abacb88dae8424f20e53093c5e1e2316753
15mm <114557943+Qniara@users.noreply.github.com>
2023-03-01 08:27:06 +0000

    rbydfcy

--- Stringi/stringi1.py
+++ Stringi/stringi1.py
@@ -2,7 +2,7 @@
 # String to taka jakby lista
 
 # input napisu
-s=input("Napisz napis: ")
+# s=input("Napisz napis: ")
 
 # print stringa
 # for x in s:
@@ -18,10 +18,25 @@ s=input("Napisz napis: ")
 # print(s,L)
 
 # przechodzenie z napisu w liste
-L=list(s)
+# L=list(s)
 # L.sort()
-print(L)
+# print(L)
 
 # przechodzenie z listy w napis
-w="-".join(L) # <- to jest juz string, wywali napis z myślnikami
-print(w)
+# w="-".join(L) # <- to jest juz string, wywali napis z myślnikami
+# print(w)
+
+# Zad.1
+#  sprawdx czy pisane slowo jest palindromem
+s=input("Napisz napis: ")
+L=list(s)
+X=L
+X.reverse()
+if X==L:
+  print("Tak")
+  a="".join(L)
+  print(a)
+else:
+  print("Nie")
+  print(L)
+  print(X)
