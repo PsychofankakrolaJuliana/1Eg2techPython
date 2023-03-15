14aa096ef47ba67a3221101546d19c62c45529a7
15mm <114557943+Qniara@users.noreply.github.com>
2023-03-15 07:40:58 +0000

    esfbdtgr

--- Algorytmy1/sortowanie_przez_wybor.py
+++ Algorytmy1/sortowanie_przez_wybor.py
@@ -1,1 +1,23 @@
 # Algorytmy1/sortowanie_przez_wybor.py
+
+# Wygeneruj losową liste 7 elementow z przedzialy od [1;20]
+# from random import randint
+# n=17
+# T=[randint(1,100) for g in range(n)]
+# print(T) 
+# for i in range(n):
+#   mi=i
+#   for jaja in range(i,n):
+#     if T[jaja]<T[mi]:
+#       mi=jaja
+#   T[i], T[mi]=T[mi], T[i]
+# print(T)
+
+# Rodzaje sortowań:
+# - Bąbelkowe
+# - Wybór 
+# - Scalanie
+# - Kubełkowe 
+# - Merge
+# - Quick sort
+# - n >> log2 n
