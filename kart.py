a58045b0e6b526116aa8bf60ca5e1ce20843a642
15mm <114557943+Qniara@users.noreply.github.com>
2023-04-17 12:09:13 +0000

    gjvbjgfc

--- kart.py
+++
@@ -1,7 +0,0 @@
-# 1
-T=[]
-n=input("Napisz wyraz: ")
-for i in n:
-  T.Append(n[i])
-for j in range(len(T)):
-  
---
+++ spr_i_kart/kart.py
@@ -0,0 +1,25 @@
+# 1
+# T=[]
+# L=[0]*150
+# n=input("Napisz wyraz: ")
+# suma=0
+# n=list(n)
+# for i in range(len(n)):
+#   T.append(n[i])
+# for j in range(len(T)):
+#   L[ord(T[j])]+=1
+# for k in range(len(L)):
+#   if L[k]>1:
+#     L[k]=1
+# for x in range(len(L)):
+#   suma+=L[x]
+# print(suma)
+
+# 2
+# n=input()
+# n=list(n)
+# T=[]
+# for i in range(len(n)):
+#   T.append(n[i])
+#   T.append(len(n)-i)
+# print(T)
