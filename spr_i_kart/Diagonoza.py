85eea6bca10b42c2c9aad017e900990f6f19cbb6
Ziemniak <114557943+Qniara@users.noreply.github.com>
2023-05-22 20:53:51 +0200

    Update Diagonoza.py

--- spr_i_kart/Diagonoza.py
+++ spr_i_kart/Diagonoza.py
@@ -1,1 +1,20 @@
+# Zad.3
+# suma=0
+# for i in range(100,1000,2):
+#   suma=suma+i
+# print(suma)
 
+# Zad.4
+# ilosc=0
+# for i in range(10,100):
+#   if i%2==0 and i%13==0:
+#     ilosc=ilosc+1
+# print(ilosc)
+
+# Zad.5
+# suma=0
+# n=int(input("Podaj liczbe: "))
+# for i in range(1,n+1):
+#   if n%i==0:
+#     suma=suma+i
+# print(suma)
