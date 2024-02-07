29b7b7dad56d2bb333e171f40ffe4b090c98b8af
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-02-07 14:05:48 +0100

    Update cw_07_02_2024.py

--- Grafy/cw_07_02_2024.py
+++ Grafy/cw_07_02_2024.py
@@ -1,13 +1,15 @@
 D = {}  #pusty slownik
 
-n, m = map(int, input().split())
+n=int(input("Daj liczbe: "))
+m=int(input("Daj druga liczbe: "))
 
 for i in range(n):
   D[i + 1] = []
   # D.update({ i+1 : [] })
 
 for _ in range(m):  #petla bez zmiennej
-  p, q = map(int, input().split())
+  p=int(input("Daj p: "))
+  q=int(input("Daj q: "))      
   D[p].append(q)
   D[q].append(p)
 # print(D)
@@ -22,17 +24,31 @@ for _ in range(m):  #petla bez zmiennej
 #     print()
 
 # policz sumę stopni wierzechołków grafu
-suma = 0
-for k,v in D:
-  suma = suma + len(v)
-print(suma)
+# suma = 0
+# for k,v in D:
+#   suma = suma + len(v)
+# print(suma)
 
 # znajdź wierzchołek o najwyższym stopniu
-maks=0
-for k,v in D:
-  if len(v)>maks:
-    maks=len(v)
-print(maks)
+# maks=0
+# for k,v in D:
+#   if len(v)>maks:
+#     maks=len(v)
+# print(maks)
 
 # znajdź wierzchołki samodzielne
+
 # Sprawdź czy uda się dojść z wierzchołka x do y
+# def DFS(n,D,vis=[]):
+#   if vis==None:
+#     vis=list()
+#     if n not in vis:
+#         vis.append(n)
+#     else:
+#       return 
+#     for nei in D[n]:
+#       DFS(nei, D, vis)
+#     return vis
+# visited=[]
+# W=DFS(1,D,visited)
+# print(W)
