827b1efc29f7fee5d7a2529177e5db464cdb7a5d
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-03-04 08:38:24 +0100

    Create cw_04_03_2024.py

---
+++ Listingi/cw_04_03_2024.py
@@ -0,0 +1,13 @@
+# Monte Carlo
+def montecarlo(n):
+  import random
+  import math
+  k=0
+  for i in range(n):
+    x=random.uniform(-1,1)
+    y=random.uniform(-1,1)
+    print(x,y)
+    if x**2+y**2<=1:
+      k+=1
+  return 4*k/n
+montecarlo(5)
