431cfb410d45f51472f2d55da5930c0632b8a97b
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-03-04 08:41:01 +0100

    Update cw_04_03_2024.py

--- Listingi/cw_04_03_2024.py
+++ Listingi/cw_04_03_2024.py
@@ -6,8 +6,11 @@ def montecarlo(n):
   for i in range(n):
     x=random.uniform(-1,1)
     y=random.uniform(-1,1)
-    print(x,y)
+    # print(x,y)
     if x**2+y**2<=1:
       k+=1
   return 4*k/n
 montecarlo(5)
+print(montecarlo(5))
+print(montecarlo(50))
+print(montecarlo(500))
