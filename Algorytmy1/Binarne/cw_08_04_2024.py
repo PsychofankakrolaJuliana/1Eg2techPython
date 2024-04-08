bdb3c7e9fbf9a3a6924957086b29876a71449bf7
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-04-08 08:26:41 +0200

    Create cw_08_04_2024.py

---
+++ Algorytmy1/Binarne/cw_08_04_2024.py
@@ -0,0 +1,73 @@
+# Zad.1
+# n = input("Podaj liczbe binarna: ")
+# if(n[len(n)]=="1"):
+#   print("Liczba jest parzysta")
+# else:
+#   print("Liczba jest nieparzysta")
+
+# x=input()
+# if(x[len(x)-1]=="0"):
+#   print("Liczba jest parzysta")
+# else:
+#   print("Liczba jest nieparzysta")
+
+# Zad.2
+
+# SUMA
+# a=input()
+# b=input()
+# if(a[len(a)]=="1" and b[len(b)]=="1"):
+#   print("parzyste")
+# if(a[len(a)]=="0" and b[len(b)]=="0"):
+#   print("parzyste")
+# else:
+#   print("nieparzyste")
+
+# c=input()
+# d=input()
+# if(c[len(c)]== d[len(d)]):
+#   print("parzyste")
+# else:
+#   print("nieparzyste")
+
+# e=input()
+# f=input()
+# if(e[-1]==f[-1]):
+#   print("parzysta")
+# else:
+#   print("nieparzysta")
+
+# ROZNICA
+# a=input()
+# b=input()
+# if(a[len(a)]=="1" and b[len(b)]=="1"):
+#   print("parzyste")
+# if(a[len(a)]=="0" and b[len(b)]=="0"):
+#   print("parzyste")
+# else:
+#   print("nieparzyste")
+
+# c=input()
+# d=input()
+# if(c[len(c)]== d[len(d)]):
+#   print("parzyste")
+# else:
+#   print("nieparzyste")
+
+# e=input()
+# f=input()
+# if(e[-1]==f[-1]):
+#   print("parzysta")
+# else:
+#   print("nieparzysta")
+
+# ILOCZYN
+# a = input()
+# b = input()
+# if (a[len(a)] == "1" or b[len(b)] == "1"):
+#   print("nieparzyste")
+# else:
+#   print("parzyste")
+
+
+# Zad.3
