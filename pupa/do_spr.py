047b515f48fd99a50ea1250482d418928e4e4937
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-04-14 13:30:41 +0200

    Update do_spr.py

--- pupa/do_spr.py
+++ pupa/do_spr.py
@@ -1,4 +1,5 @@
 # MONTE CARLO
+
 # import random
 # def monte_carlo_pi(i):
 #     a = 0
@@ -10,6 +11,29 @@
 #             a += 1
 #     pi = 4 * a / b
 #     return pi
-
 # pi = monte_carlo_pi(1000000)
 # print("Przybliżona wartość liczby π (pi) za pomocą algorytmu Monte Carlo:", pi)
+
+
+# LIDER
+
+# wersja 1
+# def lider1(ids):
+#   cunts={}
+#   for i in ids:
+#     if i in cunts:
+#       cunts[i]+=1
+#     else:
+#       cunts[i]=1;
+#   lider = max(cunts, key=cunts.get)
+#   return lider
+# pussy=[8,2,1,1,4,4,5,6,6,4,7]
+# lider_id=lider1(pussy)
+# print(lider_id)
+
+# wersja 2
+# def lider2(ids):
+#   lider=max(ids)
+#   return lider
+# cunts=[1,4,8,4,6,5,2]
+# print(lider2(cunts))
