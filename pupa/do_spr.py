f99082c37965be24a0e47d5c02965df52ee16173
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-04-14 12:55:26 +0200

    Create do_spr.py

---
+++ pupa/do_spr.py
@@ -0,0 +1,15 @@
+# MONTE CARLO
+# import random
+# def monte_carlo_pi(i):
+#     a = 0
+#     b = i
+#     for _ in range(i):
+#         x = random.uniform(0, 1)
+#         y = random.uniform(0, 1)
+#         if x**2 + y**2 <= 1:
+#             a += 1
+#     pi = 4 * a / b
+#     return pi
+
+# pi = monte_carlo_pi(1000000)
+# print("Przybliżona wartość liczby π (pi) za pomocą algorytmu Monte Carlo:", pi)
